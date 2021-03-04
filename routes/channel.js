5c31eb1c8c438e5c01a271b370febacd141fddfb
Björn Tirsén <bjorntirsen@gmail.com>
2021-03-04 18:04:22 +0100

    ✨Enable posts to channel

--- routes/channel.js
+++ routes/channel.js
@@ -1,36 +1,31 @@
-const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.use(express.urlencoded({ extended: true }));
 
-router.use('/public', express.static(path.join(__dirname, 'public')));
-
 const Channel = require('../models/channel');
 const Post = require('../models/post');
 
-function renderChannel(id, response) {
-  Channel.findById(id, (err, data) => {
-    if (err) return console.error(err);
-    response.render('channel.ejs', { channel: data });
-  });
-}
-
 router.get('/:id', (req, res) => {
-  renderChannel(req.params.id, res);
+  Channel.findById(req.params.id, (err, data) => {
+    if (err) return console.error(err);
+    res.render('channel.ejs', { channel: data });
+  });
 });
 
 router.post('/:id', (req, res) => {
-  if (req.body.content == undefined) {
-    alert('Username and Post cannot be empty');
-  } else {
-    const post = new Post({
-      by: req.body.by,
-      content: req.body.content || '',
-    });
-    //console.log(req.body);
-  }
-  renderChannel(req.params.id, res);
+  const post = new Post({
+    by: req.body.by,
+    content: req.body.content,
+  });
+  Channel.updateOne(
+    { _id: req.params.id },
+    { $push: { posts: post } },
+    (err) => {
+      if (err) return console.error(err);
+      res.redirect(`/channel/${req.params.id}`);
+    }
+  );
 });
 
 module.exports = router;
