7096e228796f83c69ca74f5539b0206087e48141
Björn Tirsén <bjorntirsen@gmail.com>
2021-03-11 09:25:48 +0100

    ♻️Removed unneccessary script

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,1 @@
-const btn = document.getElementById('submit');
-let by = document.getElementById('by');
-let content = document.getElementById('content');
-const form = document.getElementById('form');
-form.addEventListener('keyup', (e) => {
-  by.value === '' || content.value === ''
-    ? (btn.disabled = true)
-    : (btn.disabled = false);
-});
+/* socket script goes here */
